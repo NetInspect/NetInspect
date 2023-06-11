bc12ccd8aed3e33b5c6698901bbe327e95ab2ec7
Gumball2000 <tahana.perigo@gmail.com>
2023-06-12 10:10:55 +1200

    Added Traceroute & UDP Stuff

---
+++ NetInspectApp - Dev/NetInspectApp/ViewModels/tracerouteScanViewModel.cs
@@ -0,0 +1,20 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Wpf.Ui.Common.Interfaces;
+
+
+namespace NetInspectApp.ViewModels
+{
+    public partial class tracerouteScanViewModel : ObservableObject, INavigationAware
+    {
+        public void OnNavigatedTo()
+        {
+        }
+
+        public void OnNavigatedFrom()
+        {
+        }
+    }
+}
+
--- NetInspectApp - Dev/NetInspectApp/Views/Pages/udpPage.xaml.cs
+++ NetInspectApp - Dev/NetInspectApp/Views/Pages/udpPage.xaml.cs
@@ -6,6 +6,9 @@ using Wpf.Ui.Common.Interfaces;
 using NetInspectLib.Discovery;
 using System.Threading.Tasks;
 using NetInspectApp.ViewModels;
+using NetInspectLib.Scanning;
+using NetInspectLib.Types;
+using System.Linq;
 
 namespace NetInspectApp.Views.Pages
 {
@@ -38,6 +41,52 @@ namespace NetInspectApp.Views.Pages
             }
         }
 
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear existing results
+            ViewModel.Results.Clear();
+            int scannedPorts = 0;
 
+            UDPScan udpScan = new UDPScan();
+            Task<bool> scan = udpScan.DoUDPScan(udpTextBox.Text, "1024");
+            bool success = await scan;
+
+            if (success)
+            {
+                ScanProgressBar.Visibility = Visibility.Visible;
+                int totalPorts = udpScan.results.Sum(host => host.Ports.Count());
+
+                foreach (Host host in udpScan.results)
+                {
+                    foreach (Port port in host.Ports)
+                    {
+                        var row = new UDPScanResult
+                        {
+                            IpAddress = host.IPAddress.ToString(),
+                            PortNumber = port.Number,
+                            Status = port.Status.ToString(),
+                            Other = port.Name
+                        };
+                        ViewModel.Results.Add(row);
+
+                        scannedPorts++;
+                        ScanProgressBar.Value = scannedPorts * 100 / totalPorts;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No Results");
+            }
+
+            ScanProgressBar.Visibility = Visibility.Hidden;
+        }
+    }
+    public class UDPScanResult
+    {
+        public string? IpAddress { get; set; }
+        public int PortNumber { get; set; }
+        public string? Status { get; set; }
+        public string? Other { get; set; }
     }
 }
